1e1a6971a4088ea930040f496df5e524c840c070
Bart Wijnants <bartwijnants@outlook.com>
2018-09-23 12:49:32 +0200

    strict typeing

--- src/DraggableListItem.tsx
+++ src/DraggableListItem.tsx
@@ -1,27 +1,43 @@
 import React from "react";
-import { DragSource } from "react-dnd";
+import {
+  DragSource,
+  ConnectDragSource,
+  DragSourceConnector,
+  DragSourceMonitor,
+  DragSourceCollector
+} from "react-dnd";
+
+interface CollectedProps {
+  isDragging?: boolean;
+  connectDragSource?: ConnectDragSource;
+}
+
+interface Props extends CollectedProps {
+  name: string;
+}
 
 const dragSource = {
-  beginDrag(props: any) {
+  beginDrag(props: Props) {
     return {
       name: props.name
     };
   }
 };
 
-const collect = (connect: any, monitor: any) => ({
+const collect: DragSourceCollector<CollectedProps> = (
+  connect: DragSourceConnector,
+  monitor: DragSourceMonitor
+) => ({
   connectDragSource: connect.dragSource(),
   isDragging: monitor.isDragging()
 });
 
-interface Props {
-  name: string;
-  isDragging: boolean;
-  connectDragSource: any;
-}
-
 const ListItem = ({ name, isDragging, connectDragSource }: Props) =>
-  connectDragSource(<li style={{ opacity: isDragging ? 0.5 : 1 }}>{name}</li>);
+  connectDragSource
+    ? connectDragSource(
+        <li style={{ opacity: isDragging ? 0.5 : 1 }}>{name}</li>
+      )
+    : null;
 
 const DraggableListItem = DragSource("workoutType", dragSource, collect)(
   ListItem
--- src/Workout.tsx
+++ src/Workout.tsx
@@ -1,32 +1,43 @@
 import React from "react";
-import { DropTarget } from "react-dnd";
+import {
+  DropTarget,
+  DropTargetMonitor,
+  DropTargetConnector,
+  ConnectDropTarget,
+  DropTargetCollector
+} from "react-dnd";
 
-interface Props {
+interface CollectedProps {
+  connectDropTarget?: ConnectDropTarget;
+  isOver?: boolean;
+}
+
+interface WorkoutProps extends CollectedProps {
   types: string[];
   onDrop: (type: string) => void;
 }
 
 const target = {
-  drop(props: Props, monitor: any) {
+  drop(props: WorkoutProps, monitor: DropTargetMonitor) {
     props.onDrop(monitor.getItem().name);
   }
 };
 
-const collect = (connect: any, monitor: any) => ({
+const collect: DropTargetCollector<CollectedProps> = (
+  connect: DropTargetConnector,
+  monitor: DropTargetMonitor
+) => ({
   connectDropTarget: connect.dropTarget(),
   isOver: monitor.isOver()
 });
 
-interface WorkoutProps extends Props {
-  connectDropTarget: any;
-  isOver: boolean;
-}
-
 const Workout = ({ connectDropTarget, isOver, types }: WorkoutProps) =>
-  connectDropTarget(
-    <div style={{ border: isOver ? "3px solid white" : "1px solid white" }}>
-      this is your workout: {types.join(", ")}
-    </div>
-  );
+  connectDropTarget
+    ? connectDropTarget(
+        <div style={{ border: isOver ? "3px solid white" : "1px solid white" }}>
+          this is your workout: {types.join(", ")}
+        </div>
+      )
+    : null;
 
 export default DropTarget("workoutType", target, collect)(Workout);
