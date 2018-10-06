92f54cc524a0eaa51290fa230cbb9150fb836f1f
Bart Wijnants <bartwijnants@outlook.com>
2018-10-06 14:46:53 +0200

    extract uuid into separate package

--- src/Content.tsx
+++ src/Content.tsx
@@ -7,7 +7,7 @@ import SegmentComponent from "./Segment";
 import { Workout, WorkoutEvent } from "./workout/Workout";
 import DraggableCard from "./DraggableCard";
 import { AddSegmentToWorkoutEvent } from "./workout/Workout";
-import Uuid from "./Uuid";
+import Uuid from "Uuid";
 
 class Segment {
   private _name: string;
